180f43bfec3ba3877c08ff9a4fc9e1db6daf661b
thashag <thashag@tacoma.k12.wa.us>
2016-10-20 07:41:54 -0700

    Update OI.java
    Switched the 12 JoystickButton variables to an array.

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/OI.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/OI.java
@@ -32,7 +32,8 @@ public class OI {
 //	public JoystickButton driverRJB;
 //
 	public Joystick manipulator; //Gamepad
-
+	
+	//Could use an array here if natural ordering.
 	public JoystickButton manipulatorA;
 	public JoystickButton manipulatorB;
 	public JoystickButton manipulatorX;
@@ -45,19 +46,8 @@ public class OI {
 	public JoystickButton manipulatorRJB;
 	
 	public Joystick driver; //Gamepad
-
-	public JoystickButton button1;
-	public JoystickButton button2;
-	public JoystickButton button3;
-	public JoystickButton button4;
-	public JoystickButton button5;
-	public JoystickButton button6;
-	public JoystickButton button7;
-	public JoystickButton button8;
-	public JoystickButton button9;
-	public JoystickButton button10;
-	public JoystickButton button11;
-	public JoystickButton button12;
+	//Switched to an array
+	public JoystickButton[] buttons = new JoystickButton[12];
 
     public OI(){
 //        driver = new Joystick(0);
@@ -75,19 +65,22 @@ public class OI {
 
     	
     	driver 		= new Joystick(0);
-
-    	button1	 	= new JoystickButton(driver, 1);
-    	button2 	= new JoystickButton(driver, 2);
-    	button3 	= new JoystickButton(driver, 3);
-    	button4 	= new JoystickButton(driver, 4);
-    	button5		= new JoystickButton(driver, 5);
-    	button6		= new JoystickButton(driver, 6);
-    	button7 	= new JoystickButton(driver, 7);
-    	button8 	= new JoystickButton(driver, 8);
-    	button9 	= new JoystickButton(driver, 9);
-    	button10 	= new JoystickButton(driver, 10);
-    	button11 	= new JoystickButton(driver, 11);
-    	button12 	= new JoystickButton(driver, 12);
+	//i starts at zero and goes to 11 one digit at a time.
+	for(int i = 0; i < buttons.length; i++){
+		buttons[i] = new JoystickButton(driver, i + 1);
+	}
+//     	button1	 	= new JoystickButton(driver, 1);
+//     	button2 	= new JoystickButton(driver, 2);
+//     	button3 	= new JoystickButton(driver, 3);
+//     	button4 	= new JoystickButton(driver, 4);
+//     	button5		= new JoystickButton(driver, 5);
+//     	button6		= new JoystickButton(driver, 6);
+//     	button7 	= new JoystickButton(driver, 7);
+//     	button8 	= new JoystickButton(driver, 8);
+//     	button9 	= new JoystickButton(driver, 9);
+//     	button10 	= new JoystickButton(driver, 10);
+//     	button11 	= new JoystickButton(driver, 11);
+//     	button12 	= new JoystickButton(driver, 12);
     	
         manipulator = new Joystick(1);
 
@@ -103,8 +96,10 @@ public class OI {
         manipulatorRJB 		= new JoystickButton(manipulator, 10);
 
         // Set commands to buttons
-        button12.whileHeld(new MacroRecordCommand("/home/lvuser/test.autonomous"));
-        button11.whileHeld(new MacroPlayCommand("/home/lvuser/test.autonomous"));
+        //button12 -> button[11] due to 0 - 11 indexes
+	buttons[11].whileHeld(new MacroRecordCommand("/home/lvuser/test.autonomous"));
+        //button11 -> button[10] due to 0 - 11 indexes
+	buttons[10].whileHeld(new MacroPlayCommand("/home/lvuser/test.autonomous"));
 
         manipulatorLB.whileHeld(new IntakeOutCommand());
         manipulatorRB.whileHeld(new IntakeInCommand());
